da217a320d0224e23dc770dff01de83c5b0fcf65
lauta-dev <altairdiaz245@gmail.com>
2025-12-02 16:05:25 -0300

    update help

--- cmd/help.go
+++ cmd/help.go
@@ -6,12 +6,11 @@ import "fmt"
 func Help() {
 
 	dialog := `
-
 	Programa desarrollado por: https://github.com/lauta-dev
 
 --help | -help: Mostrar esta ayuda
 --env-file | -env-file: Cargar archivo .env
---active-history | -active-history: Activar historial de request
+--activate-history | -activate-history: Activar historial de request
 	`
 
 	fmt.Print(dialog)
